f950005159a8ed09b2f567a411493a13fae18265
SHubinaOlga <olga.shubina85@yandex.ru>
2024-08-21 19:28:21 +0300

    main.py

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
 calls = 0
-
-
+ 
 def count_calls():
     global calls
-
-
-calls = 4
-
-
+    calls = calls +1
+ 
 def string_info(string):
-    print(string)
-
-
-string_info1 = (len('Capybara'))
-string_info2 = ('Capybara'.upper())
-string_info3 = ('Capybara'.lower())
-string_info4 = (len('Armageddon'))
-string_info5 = ('Armageddon'.upper())
-string_info6 = ('Armageddon'.lower())
-string_info([string_info1, string_info2, string_info3])
-string_info([string_info4, string_info5, string_info6])
-
-
+    tekst_ = str(string)
+    result = len(tekst_), tekst_.upper(), tekst_.lower()
+    count_calls()
+    return result
+ 
 def is_contains(string, list_to_search):
-    list_to_search = ('Urban', ['dan', 'BaNaN', 'urBAN'])
-    list_to_search_2 = ('cycle', ['recycling', 'cyclic'])
-    string = ('Urban', 'urBAN')
-    if string[0] in list_to_search or string[0] in list_to_search_2:
-        print(True)
-    else:
-        print(False)
-    if string[1] in list_to_search or string[1] in list_to_search_2:
-        print(True)
-    else:
-        print(False)
-
-
-is_contains(True, False)
-
+    list_to_search = list(list_to_search)
+    string = str(string).lower()
+    count_calls()
+    for i in range(len(list_to_search)):
+        if str(list_to_search[i].lower()) == string:
+             result = True
+             break
+        else:
+            result = False
+            continue
+    return result
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 print(calls)
